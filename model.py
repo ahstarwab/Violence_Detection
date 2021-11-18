40f7272b79f868c4a42fc90222000abff6fb673b
ahstarwab <30588008+ahstarwab@users.noreply.github.com>
2021-11-18 15:52:37 +0900

    Update model.py

--- model.py
+++ model.py
@@ -11,15 +11,14 @@ class MYNET(nn.Module):
     def __init__(self, sequence_size):
         super().__init__()
         self.sequence_size = sequence_size
-        self.motion_features = Spatial_Attention(sequence_size+1)
+        self.SA = Spatial_Attention(sequence_size+1)
         self.backbone = EfficientNet.from_pretrained('efficientnet-b0', in_channels=3)
         self.TA = Temporal_Attention(n_segment=sequence_size//3, feature_dim=1000, num_class=2)
         
     def forward(self, rgb):
         B = rgb.shape[0]
-        SA = self.motion_features(rgb)
+        SA = self.SA(rgb)
         encoded_features = rgb[:,1:,...]*SA
         backbone_out = self.backbone(encoded_features.mean(2).reshape(-1, 3, 224, 224))
         temporal_vec = (backbone_out).reshape(B, self.sequence_size//3, -1)
-        return self.Temporal_Attention(temporal_vec)
-
+        return self.TA(temporal_vec)
